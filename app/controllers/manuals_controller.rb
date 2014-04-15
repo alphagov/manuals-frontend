57af3505c1f6d239d00860e6b37f4e5d9b680533
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-04-15 15:55:51 +0100

    Use data from some JSON
    Rather than hardcoding values in the erb.

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,7 +1,10 @@
 class ManualsController < ApplicationController
 
   def show
+    @document = JSON.parse(File.open('public/EIM11200.json').read)
+
     render text: 'Not found', status: 404 unless params[:id] == 'magic-slug'
+
   end
 
 end
