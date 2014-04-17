39f5511a38215105920569c289b25fec5537caae
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-04-17 15:18:26 +0100

    Add jasmine-rails to run JavaScript tests
    - Add `jasimine-rails` gem to `Gemfile`
    - Mount test runner on `/spec` so tests can be run in browser
    - Add `jasmine_rails.rake` rake task so that tests are run by default
      when you call `bundle exec rake`
    - Add copy of jQuery to helpers. This is normally included on pages
      using static/slimmer but won't be available in tests
    - Add config for `jasmine-rails`
    
    This commit+setup courtesy of https://github.com/alphagov/finder-frontend/commit/c3bd8b2098c20bff525b53728a9c8d67f13e4aae

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ HmrcManualsFrontend::Application.routes.draw do
   resources :manuals, only: [:show]
 
   root to: proc { [404, {}, ['Not found']] }
-
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 end
