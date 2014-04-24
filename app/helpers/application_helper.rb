9b214f1c07506bc51b29a0f80cb0ee27e8262094
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-04-24 16:37:58 +0100

    Add <div class='govspeak'> wrap
    Since all govspeak_to_html content will have a govspeak wrap, add this to the helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def govspeak_to_html(markdown)
     doc = Govspeak::Document.new markdown
-    return doc.to_html.html_safe
+    return content_tag(:div, doc.to_html.html_safe, class: 'govspeak')
   end
 
 end
