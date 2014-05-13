3c0d4026fb7a68c4aae7250345291f4209fac1bb
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-05-13 13:44:23 +0100

    Refactor @document into presenter
    Refactor document/section_group/section into presenters. Update views.

---
+++ app/presenters/document_presenter.rb
@@ -0,0 +1,72 @@
+class DocumentPresenter
+
+  def initialize(document)
+    @document = document
+  end
+
+  def section_id
+    document['details']['manual-section-id']
+  end
+
+  def title
+    document['title']
+  end
+
+  def body
+    document['details']['body']
+  end
+
+  def section_groups
+    document['details']['sections'].map do | group |
+      SectionGroupPresenter.new(group)
+    end
+  end
+
+private
+  attr_reader :document
+
+
+  class SectionGroupPresenter
+
+    def initialize(section_group)
+      @group = section_group
+    end
+
+    def title
+      group['title']
+    end
+
+    def sections
+      group['sections'].map do | section |
+        SectionPresenter.new(section)
+      end
+    end
+
+  private
+    attr_reader :group
+
+  end
+
+  class SectionPresenter
+
+    def initialize(section)
+      @section = section
+    end
+
+    def title
+      section['title']
+    end
+
+    def section_id
+      section['manual-section-id']
+    end
+
+    def body
+      section['body']
+    end
+
+  private
+    attr_reader :section
+
+  end
+end
