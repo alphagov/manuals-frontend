35630f454d716888e62cbd1fc18ba419e9ea77a1
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-05-14 15:40:14 +0100

    JS to auto open linked-to sections
    - On pageload open the section in the hash, if there is one
    - Catch any links within a govspeak block and check if they are linking to somewhere else on this page. If they are, open that section. If they aren't then allow the event to propagate.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,14 @@ jQuery(function($) {
   $('.js-collapsible-collection').each(function(){
     new GOVUK.CollapsibleCollection({el:$(this)});
   })
+
+  $('.govspeak').on('click', 'a', function(event){
+    if (window.location.pathname == event.target.pathname) {
+      $section = $(event.target.hash);
+      if (typeof(section) != undefined) {
+        new GOVUK.Collapsible($section).open();
+        event.stopPropagation();
+      }
+    }
+  })
 });
