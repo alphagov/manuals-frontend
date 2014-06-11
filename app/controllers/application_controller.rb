4de98342c290850b18b30490ec218c58e24ce734
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-06-11 10:47:13 +0100

    Add alpha label

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :slimmer_headers
-
+  before_filter :set_alpha_header
   before_filter :set_robots_headers
 
   private
 
   def slimmer_headers
     set_slimmer_headers(template: "header_footer_only")
+  end
+
+  def set_alpha_header
+    response.headers[Slimmer::Headers::ALPHA_LABEL] = "before:#manuals-frontend header"
   end
 
   def set_robots_headers
