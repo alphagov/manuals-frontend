343d93a3e3ba5eac7162af41e7456175324d5323
Andrew Mitchell <themitchell@me.com>
2014-09-04 11:27:01 +0100

    Add EIM holding page
    This in order to explain to users where the Employment Income manual has gone

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ManualsFrontend::Application.routes.draw do
 
+  # Holding page for /guidance/employment-income-manual
+  get 'guidance/employment-income-manual', to: 'static_pages#employment_income_manual'
+  get 'guidance/employment-income-manual/*', to: redirect('guidance/employment-income-manual')
+
   get '/guidance/:manual_id', to: 'manuals#index'
   get '/guidance/:manual_id/updates', to: 'manuals#updates'
   get '/guidance/:manual_id/:section_id', to: 'manuals#show'
