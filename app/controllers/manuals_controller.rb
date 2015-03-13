abfbb6965d517d6f6a035282946dc32871d601d6
Dominic Baggott <dominic.baggott@gmail.com>
2015-03-13 09:55:45 +0000

    Move manual checks to before_action filters
    Handle rendering the static holding page for EIM or a 404 for a
    non-existent manual in a before filter keeps exceptional behaviour out
    of the normal logic.

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -3,6 +3,9 @@ require 'gds_api/helpers'
 class ManualsController < ApplicationController
   include GdsApi::Helpers
 
+  before_action :render_employment_income_manual
+  before_action :ensure_manual_is_found
+
   def index
     @manual = ManualPresenter.new(manual)
   end
@@ -21,20 +24,22 @@ class ManualsController < ApplicationController
 
 private
 
+  def render_employment_income_manual
+    if manual.nil? && params["manual_id"] == "employment-income-manual"
+      render :employment_income_manual
+    end
+  end
+
+  def ensure_manual_is_found
+    error_not_found unless manual
+  end
+
   def error_not_found
     render status: :not_found, text: "404 error not found"
   end
 
   def manual
-    manual = fetch(params["manual_id"])
-    unless manual
-      if params["manual_id"] == 'employment-income-manual'
-        render :employment_income_manual
-      else
-        error_not_found
-      end
-    end
-    manual
+    fetch(params["manual_id"])
   end
 
   def fetch(manual_id, section_id = nil)
