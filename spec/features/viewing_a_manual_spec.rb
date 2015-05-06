d1c391860b60ef9d801cf31842fd8135674ca345
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-05-06 12:38:51 +0000

    Add basic contract tests for manual_sections
    Different contract tests will be needed for hmrc_manuals and
    hmrc_manual_sections.

--- spec/features/viewing_a_manual_spec.rb
+++ spec/features/viewing_a_manual_spec.rb
@@ -173,4 +173,24 @@ feature "Viewing manuals and sections" do
       end
     end
   end
+
+  describe "support for example manual sections from govuk-content-schemas" do
+    it 'should render a page including the section title' do
+      # Assumption that there is an example manual relating to every example section
+      GovukContentSchemaTestHelpers::Examples.new.get_all_for_format("manual").each do |example_json|
+        content_item = JSON.parse(example_json)
+        content_store_has_item(content_item['base_path'], content_item)
+      end
+
+      GovukContentSchemaTestHelpers::Examples.new.get_all_for_format("manual_section").each do |example_json|
+        content_item = JSON.parse(example_json)
+        content_store_has_item(content_item['base_path'], content_item)
+
+        visit content_item['base_path']
+
+        expect(page.status_code).to eq(200)
+        expect_section_title_to_be(content_item['title'])
+      end
+    end
+  end
 end
