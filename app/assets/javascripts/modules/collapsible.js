ff94a036a80a49d0b2aa62b92eb118a29cf6578e
kr8n3r <jani@kraner.dev>
2019-05-16 13:28:10 +0100

    Update with accessibility fixes
    - Update markup to use `button` instead of `a`
    - Add aria-expanded attribute to the button
    - Add a handleClick function that contains section toggle
    and button attribute toggle
    - Add aria-hidden=true attribute to the toggle arrow span -
    this is to hide it for screenreaders. It is a purely visual toggle
    mechanism and we have already made the entire heading button the toggle.

--- app/assets/javascripts/modules/collapsible.js
+++ app/assets/javascripts/modules/collapsible.js
@@ -5,11 +5,21 @@
   function Collapsible(section){
     this.$section = section;
     this.$clickTarget = this.$section.find('.js-subsection-title');
-    this.$clickTarget.on('click', this.toggle.bind(this));
+    this.$clickTarget.on('click', this.handleClick.bind(this));
+    this.$button = this.$clickTarget[0].querySelector('button');
     this.addToggle();
 
     this.$section.on('focus', '.js-subsection-body a', this.showSectionWhenLinkFocused.bind(this));
   }
+
+  Collapsible.prototype.handleClick = function handleClick(){
+    this.toggle()
+    this.toggleAriaExpanded()
+  };
+
+  Collapsible.prototype.toggleAriaExpanded = function toggleAriaExpanded() {
+    this.$button.getAttribute('aria-expanded') === 'false' ?  this.$button.setAttribute('aria-expanded', 'true') : this.$button.setAttribute('aria-expanded', 'false');
+  };
 
   Collapsible.prototype.showSectionWhenLinkFocused = function() {
     if (this.$section.is('.closed')) {
@@ -18,21 +28,22 @@
   };
 
   Collapsible.prototype.addToggle = function addToggle(){
-    var $toggleHTML = $("<span class='js-toggle'></span>");
+    var $toggleHTML = $("<span class='js-toggle' aria-hidden='true'></span>");
     this.$clickTarget.append($toggleHTML);
   };
 
-  Collapsible.prototype.toggle = function toggle(event){
+  Collapsible.prototype.toggle = function toggle(){
     this.$section.toggleClass('closed');
-    event.preventDefault();
   };
 
   Collapsible.prototype.close = function close(){
     this.$section.addClass('closed');
+    this.$button.setAttribute('aria-expanded', 'false');
   };
 
   Collapsible.prototype.open = function open(){
     this.$section.removeClass('closed');
+    this.$button.setAttribute('aria-expanded', 'true');
   };
 
   Collapsible.prototype.isClosed = function(){
