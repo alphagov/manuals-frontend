1f1ccfdbb793dc5b55b9c359e35fcbc96d41c4eb
kr8n3r <jani@kraner.dev>
2019-11-06 12:08:21 +0000

    Remove frontend_toolkit import in javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require govuk_publishing_components/all_components
 //= require_tree ./modules
-// from govuk_frontend_toolkit and not delivered by static as part of
-// header-footer-only on deployed environments
-//= require govuk/primary-links
 
 jQuery(function($) {
   "use strict";
