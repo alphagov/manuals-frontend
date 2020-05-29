154c268de3e5d872d52cd85c740d7df1b0652b7d
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-29 10:59:06 +0100

    Manually fix remaining linting issues
    This app doesn't use Cucumber.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,7 @@ require "slimmer/rspec"
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 RSpec.configure(&:infer_spec_type_from_file_location!)
 
