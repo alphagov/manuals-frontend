dff3bbc5c8c974f525a4f64923342337ea81da33
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-07-07 13:50:35 +0100

    Use individual component assets
    - individual JS
    - updates individual Sass as some was missing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
-//= require govuk_publishing_components/all_components
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/govspeak
 //= require_tree ./modules
 
 jQuery(function($) {
