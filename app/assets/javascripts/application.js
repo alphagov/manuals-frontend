ef227b71fa788935658630009cd449503f0f30c3
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2021-02-08 11:23:25 +0000

    Remove all instances of previous manual js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,13 +3,8 @@
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
-//= require_tree ./modules
 
 jQuery(function ($) {
   'use strict'
   GOVUK.primaryLinks.init('.primary-item')
-
-  $('.js-collapsible-collection').each(function () {
-    new GOVUK.CollapsibleCollection({ $el: $(this) }) // eslint-disable-line no-new
-  })
 })
